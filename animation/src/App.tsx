77e2e1440f48277e8a4b0368bdd3ee63c0ceace2
reignkk1 <ruaals456@naver.com>
2022-09-27 18:37:30 +0900

    drag
    gestures

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,10 +10,21 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const Container = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 255);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: grid;
@@ -20,42 +32,28 @@ const Box = styled(motion.div)`
   align-items: center;
 `;
 
-const Circle = styled(motion.div)`
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-  background-color: white;
-  place-self: center;
-`;
-
 const boxVariant = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVariant = {
-  start: { opacity: 0, y: 20 },
-  end: { opacity: 1, y: 0 },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(182, 227, 136)" },
 };
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
   return (
     <Wrapper>
-      <Box variants={boxVariant} initial="start" animate="end">
-        <Circle variants={circleVariant} />
-        <Circle variants={circleVariant} />
-        <Circle variants={circleVariant} />
-        <Circle variants={circleVariant} />
-      </Box>
+      <Container ref={containerRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragConstraints={containerRef}
+          variants={boxVariant}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="drag"
+        ></Box>
+      </Container>
     </Wrapper>
   );
 }
