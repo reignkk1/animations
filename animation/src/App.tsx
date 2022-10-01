77bb6b50311b6b0fdd4aa5aaf0b56bee2322274f
reignkk1 <ruaals456@naver.com>
2022-10-01 12:17:46 +0900

    Motion-values

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,24 +1,14 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
 import { useEffect } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 500vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const Container = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.3);
-  border-radius: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
 const Box = styled(motion.div)`
@@ -34,15 +24,26 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 1, 800], [2, 1, 0]);
-  useEffect(() => {
-    scale.onChange(() => {
-      console.log(scale.get());
-    });
-  }, [x]);
+  const rotate = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
   return (
-    <Wrapper>
-      <Box style={{ x, scale: scale }} drag="x" dragSnapToOrigin></Box>
+    <Wrapper style={{ background: gradient }}>
+      <Box
+        style={{ x, rotateZ: rotate, scale }}
+        drag="x"
+        dragSnapToOrigin
+      ></Box>
     </Wrapper>
   );
 }
