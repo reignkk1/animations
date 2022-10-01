39cbd6dcfcd5bec5db66ce3a83f5cb9372d604a2
reignkk1 <ruaals456@naver.com>
2022-10-01 21:52:01 +0900

    Slider

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -19,32 +19,54 @@ const Box = styled(motion.div)`
   border-radius: 50px;
   top: 100px;
   box-shadow: 0px 1px 3px black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
 `;
 
 function App() {
-  const [show, setShow] = useState(false);
-  const toggleShow = () => {
-    setShow((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextClick = () => {
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
   };
-
   const boxVariant = {
-    start: { opacity: 0, scale: 0 },
-    end: { opacity: 1, scale: 1, rotateZ: 360 },
-    leave: { opacity: 0, y: 10, scale: 0 },
+    start: { opacity: 0, x: 500, scale: 0 },
+    end: {
+      opacity: 1,
+      x: 0,
+      scale: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+    exit: {
+      opacity: 0,
+      x: -500,
+      scale: 0,
+      transition: {
+        duration: 1,
+      },
+    },
   };
   return (
     <Wrapper>
       <AnimatePresence>
-        {show ? (
-          <Box
-            variants={boxVariant}
-            initial="start"
-            animate="end"
-            exit="leave"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((prev) =>
+          prev === visible ? (
+            <Box
+              variants={boxVariant}
+              initial="start"
+              animate="end"
+              exit="exit"
+              key={prev}
+            >
+              {prev}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
-      <button onClick={toggleShow}>Click</button>
+      <button onClick={nextClick}>Click</button>
     </Wrapper>
   );
 }
