3abeb7d23c82d0886b715a18e44215e40d541fd7
reignkk1 <ruaals456@naver.com>
2022-10-02 13:52:41 +0900

    Slider

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -27,11 +27,19 @@ const Box = styled(motion.div)`
 
 function App() {
   const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+
   const nextClick = () => {
+    setBack(false);
     setVisible((prev) => (prev === 10 ? 10 : prev + 1));
   };
+  const prevClick = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+
   const boxVariant = {
-    start: { opacity: 0, x: 500, scale: 0 },
+    start: (back: boolean) => ({ opacity: 0, x: back ? -500 : 500, scale: 0 }),
     end: {
       opacity: 1,
       x: 0,
@@ -40,33 +48,32 @@ function App() {
         duration: 1,
       },
     },
-    exit: {
+    exit: (back: boolean) => ({
       opacity: 0,
-      x: -500,
+      x: back ? 500 : -500,
       scale: 0,
       transition: {
         duration: 1,
       },
-    },
+    }),
   };
+
   return (
     <Wrapper>
-      <AnimatePresence>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((prev) =>
-          prev === visible ? (
-            <Box
-              variants={boxVariant}
-              initial="start"
-              animate="end"
-              exit="exit"
-              key={prev}
-            >
-              {prev}
-            </Box>
-          ) : null
-        )}
+      <AnimatePresence custom={back}>
+        <Box
+          custom={back}
+          variants={boxVariant}
+          initial="start"
+          animate="end"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
-      <button onClick={nextClick}>Click</button>
+      <button onClick={prevClick}>Prev</button>
+      <button onClick={nextClick}>Next</button>
     </Wrapper>
   );
 }
